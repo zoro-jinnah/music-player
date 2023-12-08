69db8e79dfdbbd517a88c89f87db76b7fedd6bdb
zoro-jinnah <mohammedalijinna46@gmail.com>
2023-12-08 19:24:00 +0530

    first commit

---
+++ music-list.js
@@ -0,0 +1,72 @@
+let allMusic = [
+    {
+        id:'music-1',
+        name:'Saari Duniya Jalaa Denge',
+        artist:'B praak',
+        img:'img/1.jpg',
+        src:'music/music-1.mp3'   
+    },
+    {
+        id:'music-2',
+        name:'Guli Mata',
+        artist:'Saad Lamjarred,Shreya Ghosal',
+        img:'img/2.jpg',
+        src:'music/music-2.mp3'   
+    },
+    {
+        id:'music-3',
+        name:'Chaleya',
+        artist:'Arijit Singh, Shilpa Rao',
+        img:'img/3.jpg',
+        src:'music/music-3.mp3'   
+    },
+    {
+        id:'music-4',
+        name:'Khalasi',
+        artist:'Aditya Gadhvi, Achint',
+        img:'img/4.jpg',
+        src:'music/music-4.mp3'   
+    },
+    {
+        id:'music-5',
+        name:'Arjan Vailly',
+        artist:'Bhupinder Babbal',
+        img:'img/5.jpg',
+        src:'music/music-5.mp3'   
+    },
+    {
+        id:'music-6',
+        name:'Asai Dhosai',
+        artist:'Priya Subramani',
+        img:'img/6.webp',
+        src:'music/music-6.mp3'   
+    },
+    {
+        id:'music-7',
+        name:'Thala pola Varuma',
+        artist:'Baharathwaj, donnan,Arjun Thomas, Mahanathi Shankar',
+        img:'img/7.webp',
+        src:'music/music-7.mp3'   
+    },
+    {
+        id:'music-8',
+        name:'Such keh Raha hai',
+        artist:'KK',
+        img:'img/8.jpg',
+        src:'music/music-8.mp3'   
+    },
+    {
+        id:'music-9',
+        name:'Kattukuyilu',
+        artist:'S.P Balasubramanian,Yesudas',
+        img:'img/9.webp',
+        src:'music/music-9.mp3'   
+    },
+    {
+        id:'music-10',
+        name:'Chakku Chakku Vathikuchi',
+        artist:'Adityan, Sujatha Mohan',
+        img:'img/10.webp',
+        src:'music/music-10.mp3'   
+    }
+]
